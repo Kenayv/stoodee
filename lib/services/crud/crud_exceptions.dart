2a28428cf918f430831b298295e77875861cf5b8
Kenayv <grosicki.jan@gmail.com>
2024-03-26 15:13:31 +0100

    prepared CRUD logically for creating/deleting/updating tasks/users. removed editing tasks from todoPage

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpened implements Exception {}
+
+class DatabaseIsNotOpened implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class CouldNotCreateUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteTask implements Exception {}
+
+class CouldNotFindTask implements Exception {}
+
+class CouldNotUpdateTask implements Exception {}
